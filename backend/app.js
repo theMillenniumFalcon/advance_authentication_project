b6febf507ad8413545e85e672bd7dd068b265ba2
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-03 18:27:56 +0530

    error_hadling_done

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 require('dotenv').config({path: "./config.env"})
 const express = require('express')
 const connectDB  =require('./config/db')
+const authRoute = require('./routers/auth')
+const errorHandler = require('./middleware/error')
 // connect ot database
 connectDB()
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 3000
-const authRoute = require('./routers/auth')
-
 app.use('/api/auth', authRoute)
+// Error handler --> should be last piece of middleware
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
